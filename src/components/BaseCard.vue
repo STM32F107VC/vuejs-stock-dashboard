ee6fbf05a2810958c58d7d4f7d94e61301767d23
STM32F107VC <baer.sydney@outlook.com>
2024-09-30 17:39:00 +0200

    Added first card to board with css styling: background-color, border-radius and padding

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card">
+        <card>
+            <h1>Hello</h1>
+            <p>Hier steht dein Kontostand</p>
+        </card>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.card {
+    background-color: #011F35;
+    border-radius: 16px;
+    padding: 20px 32px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
-    <div class="new-component">
-      <!-- Your component template goes here -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: "NewComponent",
-    props: {
-      // Define your props here
-    },
-  };
-  </script>
-  
-  <style scoped>
-  /* Your component styles go here */
-  </style>
+  <div class="HomeView">
+    <!-- Your component template goes here -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeView",
+  props: {
+    // Define your props here
+  },
+};
+</script>
+
+<style scoped>
+/* Your component styles go here */
+</style>
